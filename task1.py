a8c3dd50a4f2ebb316fad963065fa10f08e3391e
wiut-tutor <wiut.tutor@gmail.com>
2020-11-24 14:44:45 +0500

    task2 added

---
+++ task1.py
@@ -0,0 +1,19 @@
+def calculate_average(marks):
+    total = 0
+    for key in marks:
+        total = total + marks[key]
+    average = total / len(marks)
+    return average
+
+
+def display_average(mark):
+    print(mark)
+
+
+def main():
+    marks = {'CSF': 75, 'FunPro': 80, 'WT': 85}
+    display_average(calculate_average(marks))
+
+
+if __name__ == '__main__':
+    main()
