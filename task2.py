6848a04c522bddd52ce5139aa4628f921d20bde4
wiut-tutor <wiut.tutor@gmail.com>
2020-11-25 12:45:18 +0500

    update

--- task2.py
+++ task2.py
@@ -1,23 +1,32 @@
 class Student:
+    """
+    __init__" is a reseved method in python classes. 
+    It is called as a constructor in object oriented terminology. 
+    This method is called when an object is created from a class and 
+    it allows the class to initialize the attributes of the class.
+    """
     def __init__(self, id, marks):
         self.id = id
         self.marks = marks
+    #The “self” is automatically assigned to the newly created instance of Student class.
 
-    def calculate_average(self):
+    def calculateAverage(self):
         total = 0
+
         for key in self.marks:
-            total = total + self.marks[key]
-        average = total / len(self.marks)
+            total = total+self.marks[key]
+        average = total/len(self.marks)
         return average
 
-    def display_average(self):
-        print(self.calculate_average())
+    def displayAverage(self):
+        print(self.calculateAverage())
 
 
-if __name__ == '__main__':
-    marks1 = {'CSF': 75, 'FunPro': 80, 'WT': 85}
-    marks2 = {'CSF': 65, 'FunPro': 70, 'WT': 75}
-    student1 = Student(1111, marks1)
-    student2 = Student(2222, marks2)
-    student1.display_average()
-    student2.display_average()
+if __name__=='__main__':
+    # The __init__() function is defined with three variables but when we are creating the Student instance, 
+    # we have to provide only two arguments: id and marks.
+    student1 = Student(1234, {'CSF': 75, 'FunPro': 80, 'WT': 85})
+    student1.displayAverage()
+
+    student2 = Student(1235, {'CSF': 74, 'FunPro': 50, 'WT': 85})
+    student2.displayAverage()
